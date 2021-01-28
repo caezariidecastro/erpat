479fe69c5313d477415d21d90eab0d52066e8b9d
timotheo123 <theolungcayana123@gmail.com>
2021-01-28 19:16:31 +0800

    Fixed Sales Matrix query

--- application/models/Sales_matrix_model.php
+++ application/models/Sales_matrix_model.php
@@ -20,7 +20,7 @@ class Sales_matrix_model extends Crud_model {
             FROM invoice_items
             LEFT JOIN deliveries ON deliveries.reference_number = invoice_items.delivery_reference_no
             LEFT JOIN inventory ON inventory.id = invoice_items.inventory_id
-            WHERE deliveries.created_on BETWEEN '$start' AND '$end'
+            WHERE deliveries.created_on BETWEEN '$start 00:00:00' AND '$end 23:59:59'
             AND inventory.item_id = $inventory_items_table.id
         ), 0) AS total_sales
         FROM $inventory_items_table
