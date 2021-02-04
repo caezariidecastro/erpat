1e8316a9acd19a4c87c4e83193b4a4ff9c642284
timotheo123 <theolungcayana123@gmail.com>
2021-02-04 18:21:36 +0800

    Added checking of email duplication on consumer creation

--- application/controllers/Consumers.php
+++ application/controllers/Consumers.php
@@ -47,6 +47,10 @@ class Consumers extends MY_Controller {
         ));
 
         $id = $this->input->post('id');
+        if ($this->Users_model->is_email_exists($this->input->post('email'))) {
+            echo json_encode(array("success" => false, 'message' => lang('duplicate_email')));
+            exit();
+        }
 
         $consumer_data = array(
             "first_name" => $this->input->post('first_name'),
