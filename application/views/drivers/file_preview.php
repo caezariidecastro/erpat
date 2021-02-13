562b6d5aba85622a5eaf434818b9d19d3a7ac634
timotheo123 <theolungcayana123@gmail.com>
2021-02-13 18:41:12 +0800

    Added background color on remove button to make it more obvious

--- application/views/drivers/file_preview.php
+++ application/views/drivers/file_preview.php
@@ -3,7 +3,7 @@
 if (isset($file_name) && $file_name) {
     echo "<div class='box saved-file-item-container'><div class='box-content pt5 pb5'>
             <img src='".base_url(get_setting("profile_image_path").$file_name)."' style='width: 100%; height: auto;'/>
-            <a href='#' style='position: absolute; right: 20px;' class='delete-saved-file p5 dark' data-file_name='$file_name'><span class='fa fa-close image-close' style='font-size: 20px;'></span></a>
+            <a href='#' style='position: absolute; right: 15px; background: #ffffff7a;' class='delete-saved-file p5 dark' data-file_name='$file_name'><span class='fa fa-close image-close' style='font-size: 20px;'></span></a>
         </div></div>";
 }
 ?>
