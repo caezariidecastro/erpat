4a6cf55812646e9ad0d4995a980f90958bce98f9
Bytes Crafter <bytescrafter@gmail.com>
2021-04-26 23:48:23 +0800

    Temporarily disable the CI3 csrf due to cors issue on post request.

--- application/config/config.php
+++ application/config/config.php
@@ -479,10 +479,10 @@ $config['global_xss_filtering'] = FALSE;
 
 
 
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'businext_csrf_token';
 $config['csrf_cookie_name'] = 'businext_csrf_cookie';
-$config['csrf_expire'] = 	43200;
+$config['csrf_expire'] = 	7200;
 $config['csrf_regenerate'] = FALSE;
 $config['csrf_exclude_uris'] = array();
 
