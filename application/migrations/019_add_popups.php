13fb7a66dfac200fb0e8878d236802f65a1530b9
BytesCrafter <bytescrafter@gmail.com>
2021-05-31 01:24:52 +0800

    Added popups used by galyon for showing notification on app start.

---
+++ application/migrations/019_add_popups.php
@@ -0,0 +1,71 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Migration_Add_popups extends CI_Migration { 
+        
+        public function up()
+        {
+                $fields = array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'constraint' => 11,
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                        ),
+                        'uuid' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 36,
+                        ),
+
+                        'title' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 255,
+                                'default' => '',
+                                'null' => true
+                        ),
+                        'description' => array(
+                                'type' => 'TEXT',
+                                'default' => '',
+                                'null' => true
+                        ),
+                        'image' => array(
+                                'type' => 'TEXT',
+                                'default' => '',
+                                'null' => true
+                        ),
+
+                        'views' => array(
+                                'type' => 'bigint',
+                                'constraint' => 20,
+                        ),
+
+                        'created_by' => array(
+                            'type' => 'INT',
+                            'constraint' => 11,
+                            'default' => '0',
+                            'null' => true
+                        ),
+                        'created_at datetime default current_timestamp',
+                        'updated_at datetime default current_timestamp on update current_timestamp',
+                        'active' => array(
+                                'type' => 'TINYINT',
+                                'constraint' => 1,
+                                'default' => '1',
+                        ),
+                        'deleted' => array(
+                            'type' => 'TINYINT',
+                            'constraint' => 1,
+                            'default' => '0',
+                        ),
+                    );
+                    $this->dbforge->add_field($fields);
+                    $this->dbforge->add_key('id', TRUE);
+                    $this->dbforge->add_key('uuid');
+
+                    $result = $this->dbforge->create_table('popups', TRUE); //IF NOT EXIST.
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('popups',TRUE); //DROP TABLE IF EXISTS table_name
+        }
+}
