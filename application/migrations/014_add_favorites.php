d5f338690b8e35bbbc2c27f623144a7f0c82e2c9
BytesCrafter <bytescrafter@gmail.com>
2021-05-30 22:18:21 +0800

    Added migration for galyon favorites

---
+++ application/migrations/014_add_favorites.php
@@ -0,0 +1,62 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); 
+
+class Migration_Add_favorites extends CI_Migration { 
+        
+        public function up()
+        {
+                $fields = array(
+                        'id' => array(
+                                'type' => 'INT',
+                                'constraint' => 11,
+                                'unsigned' => TRUE,
+                                'auto_increment' => TRUE
+                        ),
+                        'uuid' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 36,
+                        ),
+                        'user_id' => array(
+                                'type' => 'VARCHAR',
+                                'constraint' => 36,
+                        ),
+                        'stores' => array(
+                                'type' => 'text',
+                                'default' => '',
+                                'null' => true
+                        ),
+                        'products' => array(
+                                'type' => 'text',
+                                'default' => '',
+                                'null' => true
+                        ),
+                        'created_by' => array(
+                            'type' => 'INT',
+                            'constraint' => 11,
+                            'default' => '0',
+                            'null' => true
+                        ),
+                        'created_at datetime default current_timestamp',
+                        'updated_at datetime default current_timestamp on update current_timestamp',
+                        'active' => array(
+                                'type' => 'TINYINT',
+                                'constraint' => 1,
+                                'default' => '1',
+                        ),
+                        'deleted' => array(
+                            'type' => 'TINYINT',
+                            'constraint' => 1,
+                            'default' => '0',
+                        ),
+                    );
+                    $this->dbforge->add_field($fields);
+                    $this->dbforge->add_key('id', TRUE);
+                    $this->dbforge->add_key('uuid');
+
+                    $result = $this->dbforge->create_table('favorites', TRUE); //IF NOT EXIST.
+        }
+
+        public function down()
+        {
+                $this->dbforge->drop_table('favorites',TRUE); //DROP TABLE IF EXISTS table_name
+        }
+}
