47d8d12dcc2ff3b4c3255b56533f5b2ce95b7437
BytesCrafter <bytescrafter@gmail.com>
2021-07-11 12:23:13 +0800

    Added endpoint for getting all currently clocked in users.

--- application/controllers/v1/api/Attendance.php
+++ application/controllers/v1/api/Attendance.php
@@ -32,12 +32,32 @@ class Attendance extends Users {
             exit();
         }
 
+        //Check if this user id is 5 min last timein.
+
+
         $is_currently_clocked_in = $this->Attendance_model->log_time($userid, "System Generated", true);  
         echo json_encode( array(
             "success"=>true, 
-            "message"=>"Successfully clocked in.",
+            "message"=>"Successfully clocked ".($is_currently_clocked_in ? "in." : "out."),
             "stamp"=>get_my_local_time(),
             "clocked"=>$is_currently_clocked_in,
         ));      
+    }
+
+    /* get clocked in members list */
+    function clocked_in_list() {
+        $options = array(
+            "access_type" => "all", 
+            "only_clocked_in_members" => true
+        );
+        $list_data = $this->Attendance_model->get_details($options)->result();
+        
+        foreach($list_data as $item) {
+            $instance = $this->Users_model->get_details(array("id"=>$item->user_id,"deleted"=>0))->row();
+            $item->fname = $instance->first_name;
+            $item->lname = $instance->last_name;
+            $item->avatar = get_avatar($instance->image);
+        }
+        echo json_encode(array("success" => true, "data" => $list_data));
     }
 }
