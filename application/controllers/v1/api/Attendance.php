91e430da68deebffb191e30db9ec020b85befbc2
BytesCrafter <bytescrafter@gmail.com>
2021-07-11 13:07:01 +0800

    Added 5 minute rule on log time during last clocked in or last clocked out.

--- application/controllers/v1/api/Attendance.php
+++ application/controllers/v1/api/Attendance.php
@@ -29,11 +29,15 @@ class Attendance extends Users {
 
         if($user['data']->status === 'inactive' || $user['data']->deleted === '1' || !user_has_permission($authId, 'attendance')) {
             echo json_encode( array("success"=>false, "message"=>"User is currently inactive/deleted or auth user dont have permission." ) );
-            exit();
+            exit;
         }
 
         //Check if this user id is 5 min last timein.
-
+        $checking = $this->check_if_now_allowed($userid);
+        if($checking['success'] == false) {
+            echo json_encode( $checking );
+            exit;
+        }
 
         $is_currently_clocked_in = $this->Attendance_model->log_time($userid, "System Generated", true);  
         echo json_encode( array(
@@ -51,7 +55,7 @@ class Attendance extends Users {
             "only_clocked_in_members" => true
         );
         $list_data = $this->Attendance_model->get_details($options)->result();
-        
+
         foreach($list_data as $item) {
             $instance = $this->Users_model->get_details(array("id"=>$item->user_id,"deleted"=>0))->row();
             $item->fname = $instance->first_name;
@@ -59,5 +63,58 @@ class Attendance extends Users {
             $item->avatar = get_avatar($instance->image);
         }
         echo json_encode(array("success" => true, "data" => $list_data));
+    }
+
+    protected function check_if_now_allowed($userid) {
+        $options = array(
+            "user_id" => $userid,
+            "access_type" => "all", 
+            //"only_clocked_in_members" => true
+        );
+        $list_data = $this->Attendance_model->get_details($options)->result();
+
+        //Let this pass as no attendance is found.
+        if(count($list_data) == 0) {
+            return array("success" => true);
+            exit;
+        }
+
+        $now = get_current_utc_time();
+        //Check if previously clocked in.
+        if($list_data[0]->out_time == "") {
+            $last = $list_data[0]->in_time;
+            $origin = new DateTime($last);
+            $target = new DateTime($now);
+            $interval = $origin->diff($target);
+            $mindiff = (int)$interval->format("%I"); //"%H:%I:%S"
+            $secdiff = (int)$interval->format("%S");
+
+            if($mindiff < 5) { //5 minutes
+                return array(
+                    "success" => false,
+                    "message" => "You're not allowed to clockout, you need to wait for ".(5-$mindiff)." minutes and ".(60-$secdiff)." seconds since your last clocked in."
+                );
+                exit;
+            }
+            //return json_encode(array("message" => "clocked in: ".$mindiff ));
+        } else { //clocked out
+            $last = $list_data[0]->out_time;
+            $origin = new DateTime($last);
+            $target = new DateTime($now);
+            $interval = $origin->diff($target);
+            $mindiff = (int)$interval->format("%I"); //"%H:%I:%S"
+            $secdiff = (int)$interval->format("%S");
+
+            if($mindiff < 5) { //5 minutes
+                return array(
+                    "success" => false,
+                    "message" => "You're not allowed to clockin, you need to wait for ".(5-$mindiff)." minutes and ".(60-$secdiff)." seconds since your last clocked out."
+                );
+                exit;
+            }
+            //echo json_encode(array("message" => "clocked out"));
+        }
+        
+        return array("success" => true);
     }
 }
