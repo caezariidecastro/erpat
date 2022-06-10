6ea469a6ecd6635eaa37d6f15c3ccd2fce721b63
BytesCrafter <bytescrafter@gmail.com>
2022-06-10 14:13:27 +0800

    Make a minor release with initial fix on slow downs.

--- application/config/app.php
+++ application/config/app.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 //don't change or add new config in this file
 
-$config['app_version'] = '1.6.33';
+$config['app_version'] = '1.7.0';
 
 define("TOKEN_EXPIRY", 43200); //12hrs
 
