ad159d14d0a20b240695afda5079f62a668710b3
Bytes Crafter <bytescrafter@gmail.com>
2022-07-16 07:28:46 -0700

    Follow up fix using the the browser editor.

--- application/controllers/Team.php
+++ application/controllers/Team.php
@@ -188,7 +188,7 @@ class Team extends MY_Controller {
             'module_height' => 1 // height of a single module in points
         );
 		
-		$current_page = 0;
+		$current_page = 1;
         $current_col_number = 0;
 		$current_rows = 0;
 
