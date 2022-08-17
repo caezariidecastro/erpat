66d20ce0c4c84cbf1b5ef554ad1db2e390ea36bd
BytesCrafter <bytescrafter@gmail.com>
2022-08-17 15:37:40 +0800

    Make the id number default of 2.

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -14,7 +14,7 @@
     }
 
     if (!function_exists('get_id_name')) {
-        function get_id_name($id, $prefix = "", $zeros = 4) {
+        function get_id_name($id, $prefix = "", $zeros = 2) {
             return $prefix.str_pad($id, $zeros, '0', STR_PAD_LEFT);
         }
     }
