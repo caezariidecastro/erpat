0d2e108afd3eb316b0c5268ce182456842ea8352
BytesCrafter <bytescrafter@gmail.com>
2023-04-20 02:56:46 +0800

    Fixed the button for the schedule actions.

--- application/views/schedule/index.php
+++ application/views/schedule/index.php
@@ -30,7 +30,7 @@
                 {title: "<?php echo lang("sunday"); ?>"},
                 {title: "<?php echo lang("created_on"); ?>"},
                 {title: "<?php echo lang("created_by"); ?>"},
-                {title: '<i class="fa fa-bars"></i>', "class": "text-center option w100"}
+                {title: '<i class="fa fa-bars"></i>', "class": "text-center dropdown-option w100"}
             ],
             xlsColumns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             printColumns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
