707d9d1944bda44ced1a041bb6e4a4685a87a4d0
BytesCrafter <bytescrafter@gmail.com>
2023-04-20 04:02:25 +0800

    Fixed issue with inventory item brands.

--- application/migrations/20230420034900_alter_product_item_column.php
+++ application/migrations/20230420034900_alter_product_item_column.php
@@ -10,18 +10,12 @@ class Migration_Alter_product_item_column extends CI_Migration {
                                 'null' => false,
                                 'after' => 'category'
                         ),
-                        'created_by' => array(
-                                'type' => 'INT',
-                                'null' => false,
-                                'after' => 'created_on'
-                        ),
                 );
                 $this->dbforge->add_column('inventory_items', $fields);
         }
 
         public function down()
         {
-                $this->dbforge->drop_column('inventory_items', 'brand');
-                $this->dbforge->drop_column('inventory_items', 'created_by');
+                
         }
 }
