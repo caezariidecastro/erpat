1feac040c6f6c628b07827ebaebb363a409b9973
BytesCrafter <bytescrafter@gmail.com>
2023-08-01 22:33:53 +0800

    Fixed the summary of leave credits and transactions not showing if no log is made.

--- application/models/Leave_applications_model.php
+++ application/models/Leave_applications_model.php
@@ -88,37 +88,32 @@ class Leave_applications_model extends Crud_model {
     }
 
     function get_summary($options = array()) {
-        $leave_applications_table = $this->db->dbprefix('leave_applications');
         $users_table = $this->db->dbprefix('users');
         $leave_types_table = $this->db->dbprefix('leave_types');
         $leave_credits_table = $this->db->dbprefix('leave_credits');
+        $leave_applications_table = $this->db->dbprefix('leave_applications');
 
         $where = "";
 
-        $where .= " AND $leave_applications_table.status='approved'";
-
-
         $start_date = get_array_value($options, "start_date");
         $end_date = get_array_value($options, "end_date");
-
         if ($start_date && $end_date) {
-            $where .= " AND ($leave_applications_table.start_date BETWEEN '$start_date' AND '$end_date') ";
+            $leave_applications_where = " AND ($leave_applications_table.start_date BETWEEN '$start_date' AND '$end_date') ";
+            $leave_credits_where = " AND ($leave_credits_table.date_created BETWEEN '$start_date' AND '$end_date') ";
         }
 
         $applicant_id = get_array_value($options, "applicant_id");
         if ($applicant_id) {
-            $where .= " AND $leave_applications_table.applicant_id=$applicant_id";
+            $where .= " AND $users_table.id=$applicant_id";
         }
 
         $leave_type_id = get_array_value($options, "leave_type_id");
         if ($leave_type_id) {
-            $where .= " AND $leave_applications_table.leave_type_id=$leave_type_id";
+            $where .= " AND $leave_types_table.id=$leave_type_id";
         }
 
         $access_type = get_array_value($options, "access_type");
-
         if ($access_type !== "all") {
-
             $allowed_members = get_array_value($options, "allowed_members");
             if (is_array($allowed_members) && count($allowed_members)) {
                 $allowed_members = join(",", $allowed_members);
@@ -129,26 +124,31 @@ class Leave_applications_model extends Crud_model {
             if ($login_user_id) {
                 $allowed_members .= "," . $login_user_id;
             }
-            $where .= " AND $leave_applications_table.applicant_id IN($allowed_members)";
+            $where .= " AND $users_table.id IN($allowed_members)";
         }
 
-        $sql = "SELECT  SUM($leave_applications_table.total_hours) AS total_hours,
-                    ( SELECT ( SUM(IF($leave_credits_table.action='debit',$leave_credits_table.counts,0)) -
-                            SUM(IF($leave_credits_table.action='credit',$leave_credits_table.counts,0)) ) as balance
-                        FROM $leave_credits_table 
-                        WHERE $leave_credits_table.deleted=0 
-                            AND $leave_credits_table.leave_type_id=$leave_applications_table.leave_type_id 
-                            AND $leave_credits_table.user_id=$users_table.id
-                        GROUP BY user_id 
-                    ) as balance, 
-                SUM($leave_applications_table.total_days) AS total_days, MAX($leave_applications_table.applicant_id) AS applicant_id, $leave_applications_table.status,
-                CONCAT($users_table.first_name, ' ',$users_table.last_name) AS applicant_name, $users_table.image as applicant_avatar,
-                $leave_types_table.title as leave_type_title,   $leave_types_table.color as leave_type_color
-            FROM $leave_applications_table
-            LEFT JOIN $users_table ON $users_table.id= $leave_applications_table.applicant_id
-            LEFT JOIN $leave_types_table ON $leave_types_table.id= $leave_applications_table.leave_type_id        
-            WHERE $leave_applications_table.deleted=0 $where
-            GROUP BY $leave_applications_table.applicant_id, $leave_applications_table.leave_type_id";
+        $sql = "SELECT $users_table.id as applicant_id, CONCAT($users_table.first_name, ' ', $users_table.last_name) as applicant_name, $users_table.image as applicant_avatar,
+                $leave_types_table.title as leave_type_title, $leave_types_table.color as leave_type_color, 
+                $leave_types_table.required_credits as required_credits, $leave_types_table.paid as paid,
+                ( SELECT SUM($leave_applications_table.total_hours)
+                FROM $leave_applications_table
+                WHERE $leave_applications_table.applicant_id=$users_table.id 
+                    AND $leave_applications_table.leave_type_id=$leave_types_table.id 
+                    AND $leave_applications_table.deleted=0 
+                    AND $leave_applications_table.status='approved' 
+                    $leave_applications_where
+                ) as total_hours,
+                ( SELECT ( SUM(IF($leave_credits_table.action='debit',$leave_credits_table.counts,0)) - SUM(IF($leave_credits_table.action='credit',$leave_credits_table.counts,0)) ) as balance
+                    FROM $leave_credits_table 
+                    WHERE $leave_credits_table.deleted=0 
+                        AND $leave_credits_table.leave_type_id=$leave_types_table.id 
+                        AND $leave_credits_table.user_id=$users_table.id 
+                        $leave_credits_where
+                    GROUP BY user_id 
+                ) as balance
+            FROM $users_table, $leave_types_table
+            WHERE $users_table.deleted=0 AND $users_table.status='active'
+                AND $leave_types_table.deleted=0 $where";
         return $this->db->query($sql);
     }
 
